268126393d7336b6dfad6a78602134f48fd353a2
John Emanuelsson <john.3manuelsson@gmail.com>
2025-12-15 10:48:23 +0100

    solve_boundary_v fixed, code refactor, options --cheat and -q added

--- bie.py
+++ bie.py
@@ -18,19 +18,30 @@ parser.add_argument(
 parser.add_argument(
   "-N",
   type=int,
-  default=100
+  default=200
 )
 parser.add_argument(
   "-M",
   type=int,
-  default=100
+  default=200
 )
 parser.add_argument(
   "--float",
   type=int,
   default=64
 )
+parser.add_argument(
+  "-q",
+  type=int,
+  default=0
+)
+parser.add_argument(
+  "--cheat",
+  action='store_true'
+)
 args = parser.parse_args()
+N = args.N
+M = args.M
 
 ## Imports
 import time
@@ -41,10 +52,6 @@ else:
 import matplotlib.pyplot as plt
 from tqdm import tqdm # Progress bar
 # from scipy.special import hankel1
-
-## Constants
-N = args.N
-M = args.M
 
 ## Set correct dtype
 dtype = {
@@ -77,6 +84,7 @@ log10 = torch.log10
 eye = torch.eye
 diag = torch.diag
 dot = torch.dot
+
 ## Helper functions
 def abs2(z):
   return real(z * conj(z))
@@ -131,28 +139,7 @@ def mask(z):
   tt = torch.atan2(imag(z), real(z))
   return torch.where(abs2(z) <= R(tt)**2, 1, 0)
 
-## Problem specific functions
-def secret_u(r): # u at coord r
-  return exp((r.real + 0.3*r.imag)/3) * sin((0.3*r.real - r.imag)/3)
-def secret_v(r): # u at coord r
-  return exp((r.real + 0.3*r.imag)/3) * cos((0.3*r.real - r.imag)/3)
-# Boundary-values
-def g(t):
-  return secret_u(r(t))
-# R(j) = distance from origin at time t
-def R(t):
-  return 3 + cos(4*t + pi)
-def RPrim(t):
-  return -4 * sin(4*t + pi)
-def RBis(t):
-  return -16 * cos(4*t + pi)
-# r(t) = R exp(i t) = coord on boundary at time t
-def r(t):
-  return R(t) * exp(1j * to_complex(t))
-def rPrim(t):
-  return (RPrim(t) + 1j*R(t)) * exp(1j * to_complex(t))
-def rBis(t):
-  return (RBis(t) + 2j*RPrim(t) - R(t)) * exp(1j * to_complex(t))
+
 
 ## BIE-algorithms
 def solve_u(M, t, bounds):
@@ -184,17 +171,36 @@ def correct_u(M, bounds):
   return mask(x) * u
   
 def solve_boundary_v(t, t_odd):
+  N = len(t)
   dsdt_odd = abs(rPrim(t_odd))
-  h_odd = torch.linalg.solve(eye(N)/2 + 2*pi/N*kernelMat@diag(dsdt_odd), g(t_odd))
   kernelMat_odd = calcKernelMat(t_odd) 
+  h_odd = torch.linalg.solve(eye(N)/2 + 2*pi/N*kernelMat_odd@diag(dsdt_odd), g(t_odd))
 
-  x = r(t)[:, None]
-  y = r(t_odd)
-  numerator = nu(t_odd)
-  denominator = y - x
-  phi = 1/(2*pi) * imag(numerator / denominator)
-  v = torch.sum(phi * h_odd * dsdt_odd, axis=-1)*2*pi/N
+  x = r(t)
+  v = torch.zeros((N))
+  for i, t_odd_i in enumerate(t_odd):
+    y = r(t_odd_i)
+    numerator = nu(t_odd_i)
+    denominator = y - x
+    phi = 1/(2*pi) * imag(numerator / denominator)
+    v += phi * h_odd[i] * dsdt_odd[i] *2*pi/N
   return v
+
+def plot_boundary_v_integrand(t, t_odd):
+  dsdt_odd = abs(rPrim(t_odd))
+  kernelMat_odd = calcKernelMat(t_odd) 
+  h_odd = torch.linalg.solve(eye(N)/2 + 2*pi/N*kernelMat_odd@diag(dsdt_odd), g(t_odd))
+  
+
+  x = r(t)
+  v = torch.zeros((N, N))
+  for i, t_odd_i in enumerate(t_odd):
+    y = r(t_odd_i)
+    numerator = nu(t_odd)
+    denominator = y - x
+    phi = 1/(2*pi) * imag(numerator / denominator)
+    v[i] = phi * h_odd[i] * dsdt_odd[i] *2*pi/N
+  plot_mat_and_show(v)
 
 def solve_u_better(M, t, v, bounds):
   N = len(t)
@@ -228,60 +234,70 @@ def plot_mat_and_show(mat, vmin=None, vmax=None):
   plt.colorbar()
   plt.show()
 
-## BIE
-# Common linspaces and meshgrids:
+## Problem specific functions
+def secret_u(r): # u at coord r
+  return exp((r.real + 0.3*r.imag)/3) * sin((0.3*r.real - r.imag)/3)
+def secret_v(r): # u at coord r
+  return exp((r.real + 0.3*r.imag)/3) * cos((0.3*r.real - r.imag)/3)
+# Boundary-values
+def g(t):
+  return secret_u(r(t))
+# R(j) = distance from origin at time t
+def R(t):
+  return 3 + cos(4*t + pi)
+def RPrim(t):
+  return -4 * sin(4*t + pi)
+def RBis(t):
+  return -16 * cos(4*t + pi)
+# r(t) = R exp(i t) = coord on boundary at time t
+def r(t):
+  return R(t) * exp(1j * to_complex(t))
+def rPrim(t):
+  return (RPrim(t) + 1j*R(t)) * exp(1j * to_complex(t))
+def rBis(t):
+  return (RBis(t) + 2j*RPrim(t) - R(t)) * exp(1j * to_complex(t))
+
+### Excersises
+
 t = torch.linspace(-pi + 2*pi/N, pi, N, dtype=dtype)
-xVec = torch.linspace(-4, 4, M, dtype=dtype)
-yVec = torch.linspace(-4, 4, M, dtype=dtype)
-
-X, Y = torch.meshgrid(xVec, yVec)
-Z = X + 1j*Y
-
-print("Calculating kernel...")
-kernelMat = calcKernelMat(t);
-plot_mat_and_show(kernelMat)
+t_odd = t + (t[1]-t[0])/2 # Assumes equal spacing between t-values
+bounds = ((-4, 4), (-4, 4))
 
 ## Problem 1:
-bounds = ((-4, 4), (-4, 4))
-print("Solving u...")
-u = solve_u(M, t, bounds)
-u_correct = correct_u(M, bounds)
-plot_mat_and_show(u, -3, 3)
-plot_mat_and_show(u_correct, -3, 3)
-log_abs_err = log10(abs(u - u_correct))
-plot_mat_and_show(log_abs_err)
+if args.q == 1 or args.q == 0:
+  print("Calculating kernel...")
+  kernelMat = calcKernelMat(t);
+  plot_mat_and_show(kernelMat)
+  print("Solving u...")
+  u = solve_u(M, t, bounds)
+  u_correct = correct_u(M, bounds)
+  plot_mat_and_show(u, -3, 3)
+  plot_mat_and_show(u_correct, -3, 3)
+  log_abs_err = log10(abs(u - u_correct))
+  plot_mat_and_show(log_abs_err)
 
 ## Problem 2:
-t_odd = t + (t[1]-t[0])/2 # Assumes equal spacing between t-values
 v = solve_boundary_v(t, t_odd)
 v_correct = correct_boundary_v(t)
-plt.plot(t, v)
-plt.plot(t, v_correct, ":")
-plt.show()
-# Plot log-abs-error
-v2 = v + torch.mean(v_correct - v);
-log_abs_err_v = log10(abs(v - v_correct))
-plt.plot(t_odd, log_abs_err_v)
-plt.show()
-v = v2
-v = v_correct;
+
+if args.cheat:
+  v = v_correct;
+
+if args.q == 2 or args.q == 0:
+  plot_boundary_v_integrand(t, t_odd)
+  plt.plot(t, v)
+  plt.plot(t, v_correct, ":")
+  plt.show()
+  # Plot log-abs-error
+  log_abs_err_v = log10(abs(v - v_correct + torch.mean(v_correct) - torch.mean(v)))
+  plt.plot(t_odd, log_abs_err_v)
+  plt.show()
 
 ## Problem 3
-u2 = solve_u_better(M, t, v, bounds)
-
-# Plot BIE-solution
-plt.figure()
-plt.imshow(u2.T, origin = 'lower', cmap='CMRmap_r', vmin=-3, vmax=3)
-plt.colorbar()
-plt.show()
-# Plot correct solution
-plt.figure()
-plt.imshow(u_correct.T, origin = 'lower', cmap='CMRmap_r', vmin=-3, vmax=3)
-plt.colorbar()
-plt.show()
-# Plot log-abs-error
-log_abs_err = log10(abs(u2 - u_correct))
-plt.figure()
-plt.imshow(log_abs_err.T, origin = 'lower', cmap='CMRmap_r')
-plt.colorbar()
-plt.show()
+if args.q == 3 or args.q == 0:
+  u = solve_u_better(M, t, v, bounds)
+  u_correct = correct_u(M, bounds)
+  plot_mat_and_show(u, -3, 3)
+  plot_mat_and_show(u_correct, -3, 3)
+  log_abs_err2 = log10(abs(u - u_correct))
+  plot_mat_and_show(log_abs_err2)
