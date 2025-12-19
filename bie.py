a10aaf594a46836f820b1b73db76dfff051b3eaa
John Emanuelsson <john.3manuelsson@gmail.com>
2025-12-19 12:43:24 +0100

    class PolarCurve created, grad_phi is an argument

--- bie.py
+++ bie.py
@@ -72,7 +72,56 @@ else:
 import matplotlib.pyplot as plt
 from tqdm import tqdm # Progress bar
 from scipy import special
+from abc import ABC, abstractmethod
 
+# Closed polar parametic curve of the form: self.R(t) * exp(i t)
+class PolarCurve(ABC):
+  @abstractmethod
+  def R(self, t): pass
+  
+  @abstractmethod
+  def RPrim(self, t): pass
+
+  @abstractmethod
+  def RBis(self, t): pass
+
+  def r_complex(self, t):
+    return self.R(t) * exp(1j * t)
+
+  def rPrim_complex(self, t):
+    return (self.RPrim(t) + 1j*self.R(t)) * exp(1j * t)
+
+  def rBis_complex(self, t):
+    return (self.RBis(t) + 2j*self.RPrim(t) - self.R(t)) * exp(1j * t)
+
+  def r(self, t):
+    return c2v(self.r_complex(t))
+
+  def rPrim(self, t):
+    return c2v(self.rPrim_complex(t))
+
+  def rBis(self, t):
+    return c2v(self.rBis_complex(t))
+  
+  # Calculated by a 90 degree rotation of the tangent
+  def nu_complex(self, t): # nu = normal
+    return -1j * self.rPrim_complex(t) / np.abs(self.rPrim_complex(t))
+
+  def nu(self, t): # nu = normal
+    return c2v(self.nu_complex(t))
+
+  # Returns 1 if coordinate is inside otherwise nan is returned
+  def mask(self, x):
+    t = np.atan2(x[...,0], x[...,1])
+    return np.where(vecdot(x, x) <= curve.R(t)**2, 1, float('nan')) # Nan is preferable over 0 because we want a white/transparent background when using plt.imshow.
+  
+class OurCurve(PolarCurve):
+  def R(self, t):
+    return (3 + cos(4*t + pi))
+  def RPrim(self, t):
+    return (-4 * sin(4*t + pi))
+  def RBis(self, t):
+    return (-16 * cos(4*t + pi))
 
 
 ## Set correct dtype
@@ -110,7 +159,6 @@ eye = np.eye
 diag = np.diag
 vecdot = np.linalg.vecdot
 
-
 ## Helper functions
 def is_complex(x):
   return torch.is_complex(x) if args.pytorch else np.iscomplexobj(x)
@@ -122,102 +170,59 @@ def c2v(z):
   return np.stack((z.real, z.imag), axis=-1)
 
 ## Functions not specific to our problem
-# Complex normal
-# Calculated by a 90 degree rotation of the tangent
-def nu_complex(t):
-  return -1j * rPrim_complex(t) / abs(rPrim_complex(t))
-# Normal in vector form
-def nu(t):
-  return c2v(nu_complex(t))
 
-# TODO: Finish comment
-# Definition of phi depends wether we have a ... 
-if args.helm: # phi_k
-  def phi(x):
-    return -1j/4 * special.hankel1(0, k * vecnorm(x))
-else: # regular phi
-  def phi(x):
-    return 1/(2*pi) * log(vecnorm(x))
-
-if args.helm:
-  def grad_phi(x):
-    return +1j/4 * k*x/vecnorm(x)[..., np.newaxis] * special.hankel1(1, k*vecdot(x, x))[..., np.newaxis]
-else:
-  def grad_phi(x):
-    return 1/(2*pi) * x/vecdot(x, x)[..., np.newaxis]
-
-
-def kernel_non_diagonal(s, t):
-  x, y = r(s), r(t)
-  return vecdot(grad_phi(y-x), nu(t))
-
-def kernel_diagonal(t):
-  return (
-    1 / (4*pi)
-    * (-RBis(t) * R(t)  + 2*RPrim(t)**2  + R(t)**2)
-    / pow(RPrim(t)**2 + R(t)**2, 3/2)
-  )
-def calcKernelMat(t):
+def calcKernelMat(t, grad_phi, curve):
   T, S = np.meshgrid(t, t)
-  mat = kernel_non_diagonal(S, T)
-  diag = kernel_diagonal(t)
-
+  X = curve.r(S)
+  Y = curve.r(T)
+  mat = vecdot(grad_phi(Y-X), curve.nu(T))
+  diag = (
+    1 / (4*pi)
+    * (-curve.RBis(t) * curve.R(t)  + 2*curve.RPrim(t)**2  + curve.R(t)**2)
+    / pow(curve.RPrim(t)**2 + curve.R(t)**2, 3/2)
+  )
   # Insert the diagonals
   idcs = np.arange(len(t))
   mat[idcs, idcs] = diag
 
   return mat
 
-def mask(x):
-  t = np.atan2(x[...,0], x[...,1])
-  return np.where(vecdot(x, x) <= R(t)**2, 1, float('nan')) # Nan is preferable to 0 because we want a white/transparent background.
+
 
 ## BIE-algorithms
-def solve_u(kernelMat, X, t, dsdt, dt):
+def solve_u(kernelMat, X, t, dsdt, dt, g, grad_phi, curve, show_progress=True):
   N = len(t)
-  # print(np.shape(kernelMat))
-  # print("X", np.shape(X))
-  # print(np.shape(t))
-  # print("dsdt", np.shape(dsdt))
-  # print(dt)
-  # print(np.shape(eye(N)/2 + kernelMat @ diag(dsdt * dt)))
-  # print(np.shape(r(t)))
-  # print(np.shape(g(t)))
-  h = linalg.solve(eye(N)/2 + kernelMat @ diag(dsdt * dt), g(t))
-  # print("h", np.shape(h))
+  h = linalg.solve(eye(N)/2 + kernelMat @ diag(dsdt * dt), g)
 
   # Calculate u
   u = np.zeros(X[...,0].shape, dtype=h.dtype)
-  for i, t_i in enumerate(tqdm(t)):
-    y_i = r(t_i)
-    nu_i = nu(t_i)
-    kernel_val = vecdot(grad_phi(y_i - X), nu(t_i))
+  for i, t_i in enumerate(tqdm(t, disable=not show_progress)):
+    y_i = curve.r(t_i)
+    nu_i = curve.nu(t_i)
+    kernel_val = vecdot(grad_phi(y_i - X), curve.nu(t_i))
     u += kernel_val * h[i] * dsdt[i]
   u *= dt
 
-  return mask(X) * u
+  return curve.mask(X) * u
 
-def correct_u(X):
-  return mask(X) * secret_u(X)
-  
-def solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt):
-  h_odd = linalg.solve(eye(N)/2 + kernelMat_odd @ diag(dsdt_odd * dt), g(t_odd))
+def solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt, g_odd, curve):
+  h_odd = linalg.solve(eye(N)/2 + kernelMat_odd @ diag(dsdt_odd * dt), g_odd)
 
-  x = r_complex(t)
-  y = r_complex(t_odd)
+  x = curve.r_complex(t)
+  y = curve.r_complex(t_odd)
   v = np.zeros(len(t), dtype=h_odd.dtype)
   for i, t_odd_i in enumerate(t_odd):
-    numerator = nu_complex(t_odd_i)
+    numerator = curve.nu_complex(t_odd_i)
     denominator = y[i] - x
     v += 1/(2*pi) * imag(numerator / denominator) * h_odd[i] * dsdt_odd[i] * dt
   return v
 
-def solve_u_better(M, t, v, x_bounds):
+def solve_u_better(M, t, g, v, x_bounds, curve):
   N = len(t)
-  f = g(t) + 1j*v
+  f = g + 1j*v
 
-  y = r_complex(t)
-  dydt = rPrim_complex(t)
+  y = curve.r_complex(t)
+  dydt = curve.rPrim_complex(t)
 
   z1 = np.linspace(x_bounds[0], x_bounds[1], M, dtype=dtype)
   z2 = np.linspace(x_bounds[2], x_bounds[3], M, dtype=dtype)
@@ -231,11 +236,8 @@ def solve_u_better(M, t, v, x_bounds):
     numerator   += (f[i] / (y[i]-Z)) * dydt[i] * 2*pi/N
     denominator += (1    / (y[i]-Z)) * dydt[i] * 2*pi/N
   u = real(numerator / denominator)
-  return mask(c2v(Z)) * u
+  return curve.mask(c2v(Z)) * u
 
-def correct_boundary_v(t):
-  x = r(t)
-  return secret_v(x)
 
 ## Plotting functions
 # Plot A, B, and the log-abs error of A assuming B is the correct solution.
@@ -288,9 +290,17 @@ def plot_kernel_and_show(mat, extent=None, title=None):
     
 
 ## Problem specific functions
+def phi_k(x):
+  return -1j/4 * special.hankel1(0, k * vecnorm(x))
+def phi(x):
+  return 1/(2*pi) * log(vecnorm(x))
+def grad_phi(x):
+  x_dot_x = vecdot(x, x)[..., np.newaxis]
+  return 1/(2*pi) * x / x_dot_x
+def grad_phi_k(x):
+  return +1j/4 * k*x/vecnorm(x)[..., np.newaxis] * special.hankel1(1, k*vecdot(x, x))[..., np.newaxis]
 def secret_u(r): # u at coord r
   if args.helm:
-    # return phi_k(r - p)
     return special.hankel1(0, k * vecnorm(r - p))
   else:
     return exp((r[...,0] + 0.3*r[...,1])/3) * sin((0.3*r[...,0] - r[...,1])/3)
@@ -298,71 +308,47 @@ def secret_u(r): # u at coord r
 def secret_v(r): # u at coord r
   return exp((r[...,0] + 0.3*r[...,1])/3) * cos((0.3*r[...,0] - r[...,1])/3)
 # Boundary-values
-def g(t):
-  return secret_u(r(t))
-# R(j) = distance from origin at time t
-def R(t):
-  return (3 + cos(4*t + pi))
-def RPrim(t):
-  return (-4 * sin(4*t + pi))
-def RBis(t):
-  return (-16 * cos(4*t + pi))
-# r(t) = R exp(i t) = coord on boundary at time t
-def r_complex(t):
-  return R(t) * exp(1j * t)
-def rPrim_complex(t):
-  return (RPrim(t) + 1j*R(t)) * exp(1j * t)
-def rBis_complex(t):
-  return (RBis(t) + 2j*RPrim(t) - R(t)) * exp(1j * t)
-def r(t): return c2v(r_complex(t))
-def rPrim(t): return c2v(rPrim_complex(t))
-def rBis(t): return c2v(rBis_complex(t))
-# def r_vec(t):
-#   return R(t) * np.stack([cos(t), sin(t)], axis=-1)
-# def rPrim_vec(t):
-#   return (diag(RPrim(t)) + 1j*R(t)) @ np.stack([cos(t), sin(t)], axis=-1)
-# def rBis_vec(t):
-#   return (RBis(t) + 2j*RPrim(t) - R(t)) * np.stack([cos(t), sin(t)], axis=-1)
+# def g(t, curve):
+#   return secret_u(curve.r(t))
 
 ### Excersises
+curve = OurCurve()
 t = np.linspace(-pi + 2*pi/N, pi, N, dtype=dtype)
 # t = np.linspace(-pi, pi, N, dtype=dtype, endpoint=False)
 t_odd = t - (t[1]-t[0])/2 # Assumes equal spacing between t-values
 dt = 2*pi/N
-dsdt = vecnorm(rPrim(t))
-dsdt_odd = vecnorm(rPrim(t_odd))
+dsdt = vecnorm(curve.rPrim(t))
+dsdt_odd = vecnorm(curve.rPrim(t_odd))
 x_bounds = (-4, 4, -4, 4)
 t_bounds = (t[0], t[-1], t[0], t[-1])
 t_bounds_odd = (t_odd[0], t_odd[-1], t_odd[0], t_odd[-1])
 x1 = np.linspace(x_bounds[0], x_bounds[1], M, dtype=dtype)
 x2 = np.linspace(x_bounds[2], x_bounds[3], M, dtype=dtype)
 X = np.stack(np.meshgrid(x1, x2), axis=-1)
+our_grad_phi = grad_phi_k if args.helm else grad_phi
 
 # plt.plot(t, kernel_diag())
 
 ## Problem 1:
 print("Calculating kernel...")
-kernelMat = calcKernelMat(t);
-kernelMat_odd = calcKernelMat(t_odd);
+kernelMat = calcKernelMat(t, our_grad_phi, curve);
+kernelMat_odd = calcKernelMat(t_odd, our_grad_phi, curve);
 if args.q == 1 or args.q == 0:
   plot_kernel_and_show(kernelMat, t_bounds, "Kernel matrix")
   print("Solving u...")
-  u = solve_u(kernelMat, X, t, dsdt, dt)
-  u_correct = correct_u(X)
+  g = secret_u(curve.r(t))
+  u = solve_u(kernelMat, X, t, dsdt, dt, g, our_grad_phi, curve)
+  u_correct = secret_u(X) * curve.mask(X)
   plot_mat_comparison_and_show(u, u_correct, x_bounds, -1, 1)
 
-  # plot_mat_and_show(u, x_bounds, -1, 1)
-  # plot_mat_and_show(u_correct, x_bounds, -1, 1)
-  # log_abs_err = log10(abs(u - u_correct))
-  # plot_mat_and_show(log_abs_err, x_bounds)
-
 ## Problem 2:
-v = solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt)
-v_correct = correct_boundary_v(t)
+g_odd = secret_u(curve.r(t_odd))
+v = solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt, g_odd, curve)
+v_correct = secret_v(curve.r(t))
 print("v", v.shape)
 print("v_correct", v_correct.shape)
 print("t", t.shape)
-print("r t", r(t).shape)
+print("r t", curve.r(t).shape)
 
 if args.cheat:
   v = v_correct
@@ -380,11 +366,12 @@ if args.q == 2 or args.q == 0:
 
 ## Problem 3
 print("Calculating odd kernel...")
-kernelMat = calcKernelMat(t_odd);
+kernelMat = calcKernelMat(t_odd, our_grad_phi, curve);
 if args.q == 3 or args.q == 0:
   plot_kernel_and_show(kernelMat_odd, t_bounds_odd, "Odd kernel matrix")
-  u = solve_u_better(M, t, v, x_bounds)
-  u_correct = correct_u(X)
+  g = secret_u(curve.r(t))
+  u = solve_u_better(M, t, g, v, x_bounds, curve)
+  u_correct = secret_u(X) * curve.mask(X)
   plot_mat_comparison_and_show(u, u_correct, x_bounds, -3, 3)
   # plot_mat_and_show(u, x_bounds, -3, 3)
   # plot_mat_and_show(u_correct, x_bounds, -3, 3)
@@ -397,28 +384,21 @@ if args.q == 4 or args.q == 0:
   x = np.array([0, 0])
 
   Ns        = np.arange(10, 1000, 10)
-  u         = []
-  u_correct = []
-  err       = []
-  for i, N2 in enumerate(Ns): 
+  u         = np.zeros(Ns.shape)
+  u_correct = np.zeros(Ns.shape)
+  err       = np.zeros(Ns.shape)
+  for i, N2 in enumerate(tqdm(Ns)): 
     t = np.linspace(-pi, pi, N2, dtype=dtype, endpoint=False)
-    dsdt = vecnorm(rPrim(t))
-    kernelMat = calcKernelMat(t)
-    print("i", i, " N", N2)
-    print("t", t)
-    print("kernel", kernelMat)
-    print("x", x)
-    print("t", t)
-    print("dsdt", dsdt)
-    print("dt", dt)
-    print(".")
-    u        .append(solve_u(kernelMat, x, t, dsdt, dt))
-    u_correct.append(correct_u(x))
-    err      .append(log10(abs(u[i] - u_correct[i])))
+    dsdt = vecnorm(curve.rPrim(t))
+    kernelMat = calcKernelMat(t, our_grad_phi, curve)
+    g = secret_u(curve.r(t))
+    u[i] = solve_u(kernelMat, x, t, dsdt, dt, g, our_grad_phi, curve, show_progress=False)
+    u_correct[i] = secret_u(x) * curve.mask(x)
+    err[i] = log10(abs(u[i] - u_correct[i]))
 
-  plt.plot(Ns, u)
-  plt.plot(Ns, u_correct)
-  plt.plot(Ns, err)
+  plt.plot(Ns, u, label="u")
+  plt.plot(Ns, u_correct, ":", label="u_correct")
+  plt.plot(Ns, err, label="err")
   plt.show()
     
 
@@ -426,7 +406,7 @@ if args.q == 4 or args.q == 0:
 # if args.q == 4 or args.q == 0:
 #   d = np.linspace(0, 1.0, 25)
 #   D, T = np.meshgrid(d, t)  
-#   X2 = r(T) - D * nu(T)
+#   X2 = curve.r(T) - D * curve.nu(T)
 #   # plot_mat_and_show(X2[1], None, -1, 1)
 
 #   plt.plot(X2[0, :], X2[1, :])
