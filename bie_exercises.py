07eb7cdf5e373003a79aaa080fc74880534fee5a
John Emanuelsson <john.3manuelsson@gmail.com>
2025-12-21 17:09:54 +0100

    .

--- bie_exercises.py
+++ bie_exercises.py
@@ -65,11 +65,11 @@ complex_dtype = {
 }[args.float]
 print("dtypes:", dtype, ",", complex_dtype)
 
-## Helper functions
+# Helper functions
 def vecnorm(x):
   return np.sqrt(np.vecdot(x, x))
 
-## Define the boundary curve for our domain
+# Define the boundary curve for our domain
 class OurCurve(bie.PolarCurve):
   def R(self, t):
     return (3 + np.cos(4*t + np.pi))
@@ -77,8 +77,6 @@ class OurCurve(bie.PolarCurve):
     return (-4 * np.sin(4*t + np.pi))
   def RBis(self, t):
     return (-16 * np.cos(4*t + np.pi))
-
-our_curve = OurCurve()
 
 if args.helm:
   def grad_phi(x):
@@ -114,8 +112,6 @@ def plot_mat_comparison_and_show(A, B, extent=None, vmin=None, vmax=None):
   for a in ax:
     a.set_aspect('equal', adjustable='box')
     a.set_axis_off()
-    # a.set_xlabel("x1")
-    # a.set_ylabel("x2")
   plt.colorbar(im0)
   plt.colorbar(im1)
   plt.colorbar(im2)
@@ -125,52 +121,49 @@ def plot_mat_comparison_and_show(A, B, extent=None, vmin=None, vmax=None):
   plt.tight_layout()
   plt.show()
 
-# Plot kernel. If complex, we plot both real and imaginary.
-def plot_kernel_and_show(mat, extent=None, title=None):
-  fig, ax = plt.subplots(1, 2 if np.iscomplexobj(mat) else 1, squeeze = False)
-  ax = ax[0]
 
-  ax[0].set_title(title + ("(real)" if np.iscomplexobj(mat) else ""))
+# Excersises
+curve = OurCurve()
+t = np.linspace(-np.pi, np.pi, args.N, dtype=dtype, endpoint=False)
+dt = 2*np.pi/args.N
+dsdt = vecnorm(curve.rPrim(t))
+x_bounds = (-4, 4, -4, 4)
+t_bounds = (t[0], t[-1], t[0], t[-1])
+x1 = np.linspace(x_bounds[0], x_bounds[1], args.M, dtype=dtype)
+x2 = np.linspace(x_bounds[2], x_bounds[3], args.M, dtype=dtype)
+X = np.stack(np.meshgrid(x1, x2), axis=-1)
+
+# Used for exercise 2 and 3
+t_odd = t - (t[1]-t[0])/2 # Assumes equal spacing between t-values
+dsdt_odd = vecnorm(curve.rPrim(t_odd))
+t_bounds_odd = (t_odd[0], t_odd[-1], t_odd[0], t_odd[-1])
+
+## Problem 1:
+if args.q == 1:
+  print("Calculating kernel...")
+  kernelMat = bie.calcKernelMat(t, grad_phi, curve);
+  # plot_kernel_and_show(kernelMat, t_bounds, "Kernel matrix")
+  title = f"Kernel matrix, N={args.N}"
+  # Plot the kernel
+  # Note that the kernel will be complex with helmholtz
+  fig, ax = plt.subplots(1, 2 if np.iscomplexobj(kernelMat) else 1, squeeze = False)
+  ax = ax[0]
+  ax[0].set_title(title + ("(real)" if np.iscomplexobj(kernelMat) else ""))
   ax[0].set_xlabel("s")
   ax[0].set_ylabel("t")
-  im1 = ax[0].imshow(mat.real.T, origin = 'lower', cmap='turbo', extent=extent)
+  im1 = ax[0].imshow(kernelMat.real.T, origin = 'lower', cmap='turbo', extent=t_bounds)
   ax[0].axis('equal')
   fig.colorbar(im1)
-  if np.iscomplexobj(mat):
+  if np.iscomplexobj(kernelMat):
     ax[1].set_title(title + "(imag)")
     ax[1].set_xlabel("s")
     ax[1].set_ylabel("t")
-    im2 = ax[1].imshow(mat.T.imag, origin = 'lower', cmap='turbo', extent=extent)
+    im2 = ax[1].imshow(kernelMat.T.imag, origin = 'lower', cmap='turbo', extent=t_bounds)
     ax[1].axis('equal')
     fig.colorbar(im2)
   plt.tight_layout()
   plt.show()
 
-
-
-# Excersises
-curve = OurCurve()
-t = np.linspace(-np.pi + 2*np.pi/args.N, np.pi, args.N, dtype=dtype)
-# t = np.linspace(-np.pi, np.pi, args.N, dtype=dtype, endpoint=False)
-t_odd = t - (t[1]-t[0])/2 # Assumes equal spacing between t-values
-dt = 2*np.pi/args.N
-dsdt = vecnorm(curve.rPrim(t))
-dsdt_odd = vecnorm(curve.rPrim(t_odd))
-x_bounds = (-4, 4, -4, 4)
-t_bounds = (t[0], t[-1], t[0], t[-1])
-t_bounds_odd = (t_odd[0], t_odd[-1], t_odd[0], t_odd[-1])
-x1 = np.linspace(x_bounds[0], x_bounds[1], args.M, dtype=dtype)
-x2 = np.linspace(x_bounds[2], x_bounds[3], args.M, dtype=dtype)
-X = np.stack(np.meshgrid(x1, x2), axis=-1)
-
-# plt.plot(t, kernel_diag())
-
-## Problem 1:
-print("Calculating kernel...")
-kernelMat = bie.calcKernelMat(t, grad_phi, curve);
-kernelMat_odd = bie.calcKernelMat(t_odd, grad_phi, curve);
-if args.q == 1:
-  plot_kernel_and_show(kernelMat, t_bounds, "Kernel matrix")
   print("Solving u...")
   g = secret_u(curve.r(t))
   u = bie.solve_u(kernelMat, X, t, dsdt, dt, g, grad_phi, curve)
@@ -178,33 +171,33 @@ if args.q == 1:
   plot_mat_comparison_and_show(u, u_correct, x_bounds, -1, 1)
 
 ## Problem 2:
-g_odd = secret_u(curve.r(t_odd))
-v = bie.solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt, g_odd, curve)
-v_correct = secret_v(curve.r(t))
-print("v", v.shape)
-print("v_correct", v_correct.shape)
-print("t", t.shape)
-print("r t", curve.r(t).shape)
+kernelMat_odd = None
+v = None
+if args.q == 2 or args.q == 3:
+  print("Calculating kernel...")
+  kernelMat_odd = bie.calcKernelMat(t_odd, grad_phi, curve);
+  g_odd = secret_u(curve.r(t_odd))
+  v = bie.solve_boundary_v(t, t_odd, kernelMat_odd, dsdt_odd, dt, g_odd, curve)
+  v_correct = secret_v(curve.r(t))
 
-if args.cheat:
-  v = v_correct
+  if args.cheat:
+    v = v_correct # We use the correct v from now on if we are cheating
 
-if args.q == 2:
-  plt.plot(t, v, label="Approximation")
-  plt.plot(t, v_correct, ":", label="Correct")
-  plt.title("v at boundary")
-  plt.show()
-  # Plot log-abs-error
-  plt.title("v at boundary - Log-abs error")
-  log_abs_err_v = np.log10(abs(v - v_correct + np.mean(v_correct) - np.mean(v)))
-  plt.plot(t_odd, log_abs_err_v)
-  plt.show()
+  if args.q == 2:
+    plt.plot(t, v, label="Approximation")
+    plt.plot(t, v_correct, ":", label="Correct")
+    plt.title("v at boundary")
+    plt.show()
+    # Plot log-abs-error
+    plt.title("v at boundary - Log-abs error")
+    log_abs_err_v = np.log10(abs(v - v_correct + np.mean(v_correct) - np.mean(v)))
+    plt.plot(t_odd, log_abs_err_v)
+    plt.show()
 
 ## Problem 3
 print("Calculating odd kernel...")
 kernelMat = bie.calcKernelMat(t_odd, grad_phi, curve);
 if args.q == 3:
-  plot_kernel_and_show(kernelMat_odd, t_bounds_odd, "Odd kernel matrix")
   g = secret_u(curve.r(t))
   u = bie.solve_u_better(args.M, t, g, v, x_bounds, curve)
   u_correct = secret_u(X) * curve.mask(X)
@@ -214,10 +207,11 @@ if args.q == 3:
 if args.q == 4:
   # Measure the far-field error for different N. We will just measure the error at the point(0, 0)
   x = np.array([0, 0])
-
-  Ns        = np.arange(10, 10+500, 10)
-  u         = np.zeros(Ns.shape, dtype=complex_dtype)
   u_correct = secret_u(x)
+
+  # Calculate u at (0, 0) for different Ns and calculate the error
+  Ns        = np.arange(50, args.N + 10, 10)
+  u         = np.zeros(Ns.shape, dtype=complex_dtype)
   err       = np.zeros(Ns.shape)
   for i, N in enumerate(tqdm(Ns)): 
     t = np.linspace(-np.pi, np.pi, N, dtype=dtype, endpoint=False)
@@ -226,12 +220,20 @@ if args.q == 4:
     kernelMat = bie.calcKernelMat(t, grad_phi, curve)
     g = secret_u(curve.r(t))
     u[i] = bie.solve_u(kernelMat, x, t, dsdt, dt, g, grad_phi, curve, show_progress=False)
-    err[i] = np.log10(abs(u[i] - u_correct))
+    err[i] = abs(u[i] - u_correct)
 
-  b, a = np.polyfit(np.log(Ns), err, 1)
+  # Fit a curve to the error
+  # log(y) = log(a + b log N) <=>
+  # y = exp(a) * exp(b log N) <=>
+  # y = exp(a) * N^b
+  b, a = np.polyfit(np.log(Ns), np.log(err), 1)
+  
+  # Plot the error
   plt.show()
-  plt.plot(Ns, err, label="Log-abs error")
-  plt.plot(Ns, a + b*np.log(Ns), label=f"{a} - {-b} log(N)")
-  plt.xscale("log")
+  plt.plot(Ns, err, label="Absolute error")
+  plt.plot(Ns, np.exp(a) * Ns**b, label=f"{np.exp(a)} * N^{b}")
+  # plt.xscale("log")
+  plt.yscale("log")
+  plt.xlabel("N")
   plt.legend()
   plt.show()
